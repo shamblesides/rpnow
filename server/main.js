317acf1ddf15e709fdf49c597d0a99b17189fb48
Nigel Nelson <nigel.mark.nelson@gmail.com>
2020-06-10 14:43:18 -0600

    move rp router into separate file

--- server/main.js
+++ server/main.js
@@ -7,13 +7,12 @@ const path = require('path');
 const os = require('os');
 const upload = require('multer')({ dest: os.tmpdir() });
 const version = require('../package.json').version
-const { generateTextFile } = require('./txt-file');
 const Store = require('./storage');
 const validate = require('./validate-user-documents');
 const Auth = require('./auth');
-const discordWebhooks = require('./discord-webhooks');
 const fetch = require('node-fetch');
 const config = require('./config');
+const rp = require('./rp');
 
 console.info(`RPNow Server ${version}`);
 
@@ -149,6 +148,11 @@ function getContext(req) {
   }
 }
 
+const addContext = (req, res, next) => {
+  req.ctx = getContext(req);
+  next();
+}
+
 function getDBFilepath(req) {
   if (config.isDemoMode) {
     return path.resolve(os.tmpdir(), `rpdemo-${req.user.userid}`)
@@ -176,12 +180,6 @@ function writeAuditLog(req, text) {
   const timestamp = new Date().toUTCString();
   fs.appendFileSync(filepath, `${timestamp} - ${text}\n`);
 }
-
-/**
- * Router pertaining to day-to-day RP interaction
- */
-const rp = new express.Router();
-api.use('/rp', rp);
 
 /**
  * Different behavior for auth/setup for demo vs non-demo
@@ -255,26 +253,24 @@ if (!config.isDemoMode) {
     fs.createReadStream(filepath).pipe(res);
   })
 
-  rp.use(cookieParser(), authMiddleware);
-
-  rp.use((req, res, next) => {
+  const maybeRedirectToSetup = (req, res, next) => {
     if (!getContext(req)) {
       return res.sendStatus(204);
     }
     next();
-  })
+  }
+
+  api.use('/rp', cookieParser(), authMiddleware, maybeRedirectToSetup, addContext, rp);
 } else {
-  api.get('/audit', cookieParser(), (req, res, next) => {
+  api.get('/audit', (req, res, next) => {
     res.type('text/plain');
     res.send('Audit logs not enabled in demo mode.')
   })
   
-  rp.use(cookieParser(), Auth.demo.middleware());
-  
   /**
    * Catch UnauthorizedError and create demo session userid if we don't have one yet
    */
-  rp.use((err, req, res, next) => {
+  const autoAuth = (err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
       const credentials = Auth.demo.generateToken();
       res.cookie('usertoken', credentials.token, {
@@ -290,9 +286,9 @@ if (!config.isDemoMode) {
     } else {
       next(err);
     }
-  });
+  };
   
-  rp.use((req, res, next) => {
+  const populateRP = (req, res, next) => {
     const dbFilepath = getDBFilepath(req);
     if (!fs.existsSync(dbFilepath)) {
       fs.writeFileSync(dbFilepath, '', { flag: 'wx' });
@@ -323,7 +319,9 @@ if (!config.isDemoMode) {
       })
     }
     next();
-  });
+  };
+
+  api.use('/rp', cookieParser(), Auth.demo.middleware(), autoAuth, populateRP, addContext, rp);
 }
 
 /**
@@ -336,218 +334,6 @@ api.post('/logout', (req, res, next) => {
     maxAge: 0,
   });
   res.redirect('/');
-});
-
-/**
- * Get RP info
- * Current state, followed by stream of messages
- * Newline-separated JSONs (expect whitespace that is sent for heartbeat)
- * Can get the current chat stream, or a particular page
- * (a page is also a stream, since it can be edited)
- */
-rp.get('/', (req, res, next) => {
-  const { Msgs, Charas, Users, getTitle, subscribe } = getContext(req);
-  
-  const page = parseInt(req.query.page) || null;
-  
-  const limit = page ? config.pageLength : config.chatScrollback;
-  const pageCount = Math.ceil(Msgs.count() / config.pageLength) || 1;
-  
-  if (page !== null && !(page >= 1 && page <= pageCount)) {
-    throw new RangeError('invalid page');
-  }
-
-  if (!Users.has(req.user.userid)) {
-    Users.put({ _id: req.user.userid, name: `User ${Math.random().toString().slice(-3)}` });
-  }
-  
-  res.type('text/plain');
-  
-  // Send big blank because an empty or very small RP will
-  // get buffered for some reason
-  res.write(Array(10000).fill(' ').join(''));
-  
-  const send = (obj) => {
-    res.write(JSON.stringify(obj)+'\n');
-  }
-  
-  const msgs = (page == null)
-    ? Msgs.list({ reverse: true, limit }).reverse()
-    : Msgs.list({ skip: (page-1)*limit, limit });
-  const charas = Charas.list();
-  const users = Users.list();
-  const title = getTitle();
-  
-  send({
-    type: 'init',
-    data: { msgs, charas, title, users, pageCount }
-  });
-  
-  function onUpdate(update) {
-    // only allow new messages to pass through
-    // OR messages that are on-screen that have been edited
-    if (update.type === 'msgs') {
-      if (msgs.find(msg => msg._id === update.data._id)) {
-        // meh... maybe replace it if we centralize this logic later
-      } else if (msgs.length === 0 || update.data._id > msgs[msgs.length-1]._id) {
-        if (msgs.length == limit) {
-          // drop new messages if we're on a specific page that has been filled
-          if (page) return;
-          // otherwise scroll past the least recent msg
-          else msgs.shift();
-        }
-        msgs.push(update.data);
-      } else {
-        return;
-      }
-    }
-    send(update);
-  }
-  
-  const unsubscribe = subscribe(onUpdate);
-  
-  var heartbeatTimer = setInterval(() => {
-    res.write(' ');
-  }, 15000);
-  
-  res.on("close", () => {
-    clearInterval(heartbeatTimer);
-    unsubscribe();
-  });
-});
-
-/**
- * Get and download a .txt file for an entire RP
- */
-rp.post('/download.txt', (req, res, next) => {
-  const { Msgs, Charas, getTitle } = getContext(req);
-
-  const msgs = Msgs.iterator();
-  const charas = Charas.list();
-  const title = getTitle();
-  const { includeOOC = false } = req.query;
-
-  const str = generateTextFile({ title, msgs, charas, includeOOC });
-  
-  res.attachment(`${title}.txt`)
-  res.type('text/plain');
-  res.send(str);
-});
-
-/**
- * Export database
- */
-rp.post('/export', (req, res, next) => {
-  const { getTitle, /*dbFilepath*/ } = getContext(req);
-  const title = getTitle();
-  const dbFilepath = getDBFilepath(req);
-  
-  res.attachment(`${title}.rprecord`);
-  res.sendFile(dbFilepath);
-});
-
-/**
- * Add/update msg
- */
-rp.put('/msgs', express.json(), (req, res, next) => {
-  const { Msgs, Webhooks, Charas, getTitle } = getContext(req);
-  
-  const { userid } = req.user;
-  const timestamp = new Date().toISOString();
-  const obj = { ...req.body, userid, timestamp };
-  
-  if (req.body._id && Msgs.has(req.body._id) && Msgs.find(req.body._id).userid !== userid) {
-    return res.status(403).json({ error: "Not allowed: tried to modify someone else's message" });
-  }
-
-  const [doc] = Msgs.put(obj);
-
-  res.json(doc);
-  
-  if (!obj._id) { // if adding a new msg, send webhook 
-    try {
-      const webhooks = Webhooks.list().map(obj => obj.webhook);
-      const chara = obj.type === 'text' && obj.who.startsWith('c-') && Charas.find(obj.who);
-      const title = getTitle();
-      discordWebhooks.send(webhooks, title, obj, chara);
-    } catch (err) {
-      // don't crash client response, just log it
-      console.info(err);
-    }
-  }
-});
-
-/**
- * Add/update chara
- */
-rp.put('/charas', express.json(), (req, res, next) => {
-  const { Charas } = getContext(req);
-
-  const { userid } = req.user;
-  const timestamp = new Date().toISOString();
-  const obj = { ...req.body, userid, timestamp };
-  
-  if (req.body._id && Charas.has(req.body._id) && Charas.find(req.body._id).userid !== userid) {
-    return res.status(403).json({ error: "Not allowed: tried to modify someone else's character" });
-  }
-
-  const [doc] = Charas.put(obj);
-
-  res.json(doc);
-});
-
-/**
- * Add webhook
- */
-rp.post('/webhook', express.urlencoded({ extended: false }), (req, res, next) => {
-  const { Webhooks } = getContext(req);
-  const { userid } = req.user;
-  const { webhook } = req.body;
-  
-  if (Webhooks.list().find(doc => doc.webhook === webhook)) {
-    throw new Error('That webhook was already added');
-  }
-  
-  Webhooks.put({ webhook, userid });
-  
-  res.redirect('/settings.html');
-});
-
-/**
- * Update RP title
- */
-rp.post('/title', express.urlencoded({ extended: false }), (req, res, next) => {
-  const { setTitle } = getContext(req);
-  
-  setTitle(req.body.title);
-  res.redirect('/settings.html');
-});
-
-/**
- * Change my username
- */
-rp.put('/username', express.json(), (req, res, next) => {
-  const { Users } = getContext(req);
-  const userid = req.user.userid;
-  const name = req.body.name;
-  
-  Users.put({ _id: userid, name });
-  res.sendStatus(204);
-})
-
-/**
- * Get a message's edit history
- */ 
-rp.get('/msgs/:doc_id([a-z0-9-]+)/history', (req, res, next) => {
-  const { Msgs, Users } = getContext(req);
-  const _id = req.params.doc_id;
-  const msgs = Msgs.history(_id);
-  msgs.forEach(msg => {
-    // add extra username prop for convenience
-    const user = Users.find(msg.userid);
-    if (user) msg.username = user.name;
-  });
-  res.json(msgs);
 });
 
 /**
